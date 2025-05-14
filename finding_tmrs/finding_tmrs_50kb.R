c7eb9db4b73b68af0237aaa4bb21acf1b4d58513
Richard Heery <qp24520@login-01.apocrita>
2025-05-14 18:06:17 +0100

    Updated finding_tmrs_50kb.R

--- finding_tmrs/finding_tmrs_50kb.R
+++ finding_tmrs/finding_tmrs_50kb.R
@@ -6,31 +6,60 @@ library(dplyr)
 library(ggplot2)
 
 # Create a BPPARAM object
-bpparam = BiocParallel::MulticoreParam(10)
+bpparam = BiocParallel::MulticoreParam(1)
 
 # Load methylation-correlation results for CPGEA normal. Took 1 minute. 
-system.time({cpgea_normal_cor_results = readRDS("cpgea_normal_whole_gene_body_correlations_50kb.rds")})
+system.time({cpgea_normal_cor_results = readRDS("new_meth_transcript_cors/cpgea_normal_whole_gene_body_correlations.rds")})
+
+# Find TMRs for CPGEA normal samples. Took 20 minutes with 1 core.
+system.time({cpgea_normal_tmrs = findTMRs(correlation_list = cpgea_normal_cor_results, 
+  p_adjust_method = "fdr", p_value_threshold = 0.05, BPPARAM = bpparam)})
+saveRDS(cpgea_normal_tmrs, "new_tmr_granges/cpgea_normal_tmrs.rds")
+rm(cpgea_normal_cor_results); gc()
+
+# Load methylation-correlation results for CPGEA tumour. Took 3 minutes. 
+system.time({cpgea_tumour_cor_results = readRDS("new_meth_transcript_cors/cpgea_tumour_whole_gene_body_correlations.rds")})
+
+# Find TMRs for CPGEA tumour samples. Took 25 minutes with 1 core.
+system.time({cpgea_tumour_tmrs = findTMRs(correlation_list = cpgea_tumour_cor_results, 
+  p_adjust_method = "fdr", p_value_threshold = 0.05, BPPARAM = bpparam)})
+saveRDS(cpgea_tumour_tmrs, "new_tmr_granges/cpgea_tumour_tmrs.rds")
+rm(cpgea_tumour_cor_results); gc()
+
+# Load methylation-correlation results for MCRPC. Took 3 minutes. 
+system.time({mcrpc_cor_results = readRDS("new_meth_transcript_cors/mcrpc_whole_gene_body_correlations.rds")})
+
+# Find TMRs for MCRPC samples. Took 17 minutes with 1 core.
+system.time({mcrpc_tmrs = findTMRs(correlation_list = mcrpc_cor_results, 
+  p_adjust_method = "fdr", p_value_threshold = 0.05, BPPARAM = bpparam)})
+saveRDS(mcrpc_tmrs, "new_tmr_granges/mcrpc_tmrs.rds")
+rm(mcrpc_cor_results); gc()
+
+### Repeat for 50 KB
+
+# Load methylation-correlation results for CPGEA normal. Took 1 minute. 
+system.time({cpgea_normal_cor_results = readRDS("new_meth_transcript_cors/cpgea_normal_whole_gene_body_correlations_50kb.rds")})
 
 # Find TMRs for CPGEA normal samples. Took 20 minutes with 1 core.
 system.time({cpgea_normal_tmrs_50kb = findTMRs(correlation_list = cpgea_normal_cor_results, 
   p_adjust_method = "fdr", p_value_threshold = 0.05, BPPARAM = bpparam)})
-saveRDS(cpgea_normal_tmrs_50kb, "tmr_granges/cpgea_normal_tmrs_50kb.rds")
+saveRDS(cpgea_normal_tmrs_50kb, "new_tmr_granges/cpgea_normal_tmrs_50kb.rds")
 rm(cpgea_normal_cor_results); gc()
 
 # Load methylation-correlation results for CPGEA tumour. Took 3 minutes. 
-system.time({cpgea_tumour_cor_results = readRDS("cpgea_tumour_whole_gene_body_correlations_50kb.rds")})
+system.time({cpgea_tumour_cor_results = readRDS("new_meth_transcript_cors/cpgea_tumour_whole_gene_body_correlations_50kb.rds")})
 
 # Find TMRs for CPGEA tumour samples. Took 25 minutes with 1 core.
 system.time({cpgea_tumour_tmrs_50kb = findTMRs(correlation_list = cpgea_tumour_cor_results, 
   p_adjust_method = "fdr", p_value_threshold = 0.05, BPPARAM = bpparam)})
-saveRDS(cpgea_tumour_tmrs_50kb, "tmr_granges/cpgea_tumour_tmrs_50kb.rds")
+saveRDS(cpgea_tumour_tmrs_50kb, "new_tmr_granges/cpgea_tumour_tmrs_50kb.rds")
 rm(cpgea_tumour_cor_results); gc()
 
 # Load methylation-correlation results for MCRPC. Took 3 minutes. 
-system.time({mcrpc_cor_results = readRDS("mcrpc_whole_gene_body_correlations_50kb.rds")})
+system.time({mcrpc_cor_results = readRDS("new_meth_transcript_cors/mcrpc_whole_gene_body_correlations_50kb.rds")})
 
 # Find TMRs for MCRPC samples. Took 17 minutes with 1 core.
 system.time({mcrpc_tmrs_50kb = findTMRs(correlation_list = mcrpc_cor_results, 
   p_adjust_method = "fdr", p_value_threshold = 0.05, BPPARAM = bpparam)})
-saveRDS(mcrpc_tmrs_50kb, "tmr_granges/mcrpc_tmrs_50kb.rds")
+saveRDS(mcrpc_tmrs_50kb, "new_tmr_granges/mcrpc_tmrs_50kb.rds")
 rm(mcrpc_cor_results); gc()
