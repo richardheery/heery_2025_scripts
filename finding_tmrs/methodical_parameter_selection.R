19a82845c89e5ab1469994f33810826fe76ed28c
Richard Heery <richardheery@gmail.com>
2025-06-30 22:51:59 +0100

    Added scripts for parameter sectlion and sample size testing

--- finding_tmrs/methodical_parameter_selection.R
+++ finding_tmrs/methodical_parameter_selection.R
@@ -16,10 +16,49 @@ parameters = setNames(expand.grid(offset_length, smoothing_factor), c("offset_le
 system.time({mcrpc_cor_results = readRDS("meth_transcript_cors/mcrpc_whole_gene_body_correlations.rds")})
 
 # Create a BPPARAM object
-bpparam = BiocParallel::MulticoreParam(4)
+bpparam = BiocParallel::MulticoreParam(20)
 
-system.time({tmr_list = lapply(seq.int(nrow(parameters)[1]), function(x)
-  subsetByOverlaps(findTMRs(correlation_list = mcrpc_cor_results, p_adjust_method = "fdr", p_value_threshold = 0.05, 
-    offset_length = parameters$offset_length[x], smoothing_factor = parameters$smoothing_factor[x], BPPARAM = bpparam), unmappable_regions))})
+system.time({foreach(i = seq.int(nrow(parameters)), .packages = c("methodical", "GenomicRanges")) %do% {
+  
+  print(i)
+  tmrs = findTMRs(correlation_list = mcrpc_cor_results, p_adjust_method = "fdr", p_value_threshold = 0.05, 
+    offset_length = parameters$offset_length[i], smoothing_factor = parameters$smoothing_factor[i], BPPARAM = bpparam)
+  
+  tmrs = subsetByOverlaps(tmrs, unmappable_regions, invert = T)
+  saveRDS(tmrs, paste0("methodical_parameter_tmrs/tmrs_", paste(unlist(parameters[i, ]), collapse = "_"), ".rds"))
+  
+}})
 
+# Read in TMRs for all combinations of parameters
+tmr_parameter_combination_list = list.files("methodical_parameter_tmrs", full.names = T)
+names(tmr_parameter_combination_list) = gsub("tmrs_", "", tools::file_path_sans_ext(basename(tmr_parameter_combination_list)))
+tmr_parameter_combination_list = lapply(tmr_parameter_combination_list, readRDS)
 
+# Load CPGEA tumour WGBS and RNA-seq data
+cpgea_meth_rse = HDF5Array::loadHDF5SummarizedExperiment("../auxillary_data/methylation_data/cpgea_meth_rse")
+cpgea_kallisto_deseq2_counts = data.frame(data.table::fread("../auxillary_data/rnaseq_data/cpgea_transcript_counts.tsv.gz"), row.names = 1)
+common_cpgea_tumour_samples = grep("T", intersect(names(cpgea_kallisto_deseq2_counts), colnames(cpgea_meth_rse)), value = T)
+
+# Make a function which will evaluate TMRs in CPGEA tumour samples
+evaluate_tmrs_in_cpgea_tumours = function(tmrs){
+  
+  results = calculateRegionMethylationTranscriptCors(
+    genomic_regions = tmrs, genomic_region_names = tmrs$tmr_name, samples_subset = common_cpgea_tumour_samples,
+    genomic_region_transcripts = tmrs$ID, meth_rse = cpgea_meth_rse, transcript_expression_table = cpgea_kallisto_deseq2_counts, 
+    cor_method = "s", BPPARAM = bpparam)
+  
+  results
+  
+}
+
+#bpparam = BiocParallel::SerialParam()
+bpparam = BiocParallel::MulticoreParam(12)
+system.time({tmr_parameter_combination_list_evaluation_results = lapply(tmr_parameter_combination_list, evaluate_tmrs_in_cpgea_tumours)})
+saveRDS(tmr_parameter_combination_list_evaluation_results, "tmr_parameter_combination_list_evaluation_results.rds")
+
+bpparam = BiocParallel::SerialParam()
+bpparam = BiocParallel::MulticoreParam(10)
+system.time({test = calculateRegionMethylationTranscriptCors(
+  genomic_regions = x, genomic_region_names = x$tmr_name, samples_subset = common_cpgea_tumour_samples,
+  genomic_region_transcripts = x$ID, meth_rse = cpgea_meth_rse, transcript_expression_table = cpgea_kallisto_deseq2_counts, 
+  cor_method = "s", BPPARAM = bpparam)})
---
+++ finding_tmrs/sample_size_test.R
@@ -0,0 +1,53 @@
+# Calcualte number of TMRs found with different numbers of samples
+
+# Load required packages
+library(methodical)
+library(dplyr)
+library(foreach)
+
+# Get TSS Granges
+tss_gr = readRDS("../auxillary_data/cage_supported_gencode_tss.rds")
+transcripts_gr = readRDS("../auxillary_data/pc_transcripts_gr.rds")[names(tss_gr)]
+
+# Expand transcripts_gr 5 KB upstream and downstream
+transcripts_gr = methodical:::expand_granges(transcripts_gr, 5000, 5000)
+
+# Load CPGEA methylation RSE and transcript counts
+cpgea_meth_rse = HDF5Array::loadHDF5SummarizedExperiment("../auxillary_data/methylation_data/cpgea_meth_rse")
+cpgea_kallisto_deseq2_counts = data.frame(data.table::fread("../auxillary_data/cpgea_normalized_kallisto_pcg_counts.tsv.gz"), row.names = 1)
+
+# Mask CpGs with less than 10 reads covering them
+assay(cpgea_meth_rse, 2)[is.na(assay(cpgea_meth_rse, 2))] = 0
+assay(cpgea_meth_rse, 1)[assay(cpgea_meth_rse, 2) < 10] = NA
+
+# Get CPGEA normal and tumour samples
+normal_samples = grep("N", intersect(names(cpgea_kallisto_deseq2_counts), colnames(cpgea_meth_rse)), value = T)
+tumour_samples = grep("T", intersect(names(cpgea_kallisto_deseq2_counts), colnames(cpgea_meth_rse)), value = T)
+
+# Create a bpparam object
+bpparam = BiocParallel::SnowParam(workers = 10)
+
+# Create 10 random samples each for sample sizes of 20, 40, 60, 80 and 100
+set.seed(123)
+random_samples = setNames(lapply(seq(20, 100, 20), function(x) 
+  setNames(lapply(1:10, function(y) sample(normal_samples, x)), paste0("_", 1:10))), seq(20, 100, 20))
+random_samples = unlist(random_samples, recursive = F)
+
+system.time({random_sample_tmrs = foreach(sample_subset = random_samples[1:4], i = names(random_samples), .packages = "methodical") %do% {
+  
+  print(i)
+  
+  # Calculate correlations using sample
+  sample_subset_cors = calculateMethSiteTranscriptCors(meth_rse = cpgea_meth_rse, 
+    transcript_expression_table = cpgea_kallisto_deseq2_counts, samples_subset = sample_subset, tss_gr = tss_gr, tss_associated_gr = transcripts_gr, 
+    cor_method = "spearman", min_number_complete_pairs = 20, BPPARAM = bpparam, add_distance_to_region = T)
+  
+  # Find TMRs for cors
+  sample_subset_tmrs = findTMRs(correlation_list = sample_subset_cors, 
+    p_adjust_method = "fdr", p_value_threshold = 0.05, BPPARAM = bpparam)
+  
+  sample_subset_tmrs
+  
+}})
+saveRDS(random_sample_tmrs, "random_sample_tmrs.rds")
+
